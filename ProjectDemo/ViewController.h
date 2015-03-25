43c2ecca3f570e1719d3a3c2e0550bd17d3e8064
dse12345z <dse12345z@gmail.com>
2015-03-25 14:09:21 +0800

    Initial Commit

---
+++ ProjectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectDemo
+//
+//  Created by daisuke on 2015/3/25.
+//  Copyright (c) 2015年 dse12345z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ProjectDemo
+//
+//  Created by daisuke on 2015/3/25.
+//  Copyright (c) 2015年 dse12345z. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
